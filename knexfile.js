5e02f8e2d8924aad5a20b6f4408bccc114b83c4f
Adan <jonesadan@yahoo.com>
2024-04-25 21:10:35 -0400

    Updated migration files and created a seed directory

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,16 @@ const { DATABASE_URL } = process.env;
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      user : 'dev_jn4u_user',
+      password : '32k9f7xYVWQL9yy6tEkRVxU9d6t7JGuF',
+      database : 'thinkful_books'
+  },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
 };
