5ffb8dec833d222697fb8ab7d5558036bc64fe54
Adan <jonesadan@yahoo.com>
2024-05-01 20:24:11 -0400

    updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,18 +5,12 @@ const { DATABASE_URL } = process.env;
 module.exports = {
   development: {
     client: "postgresql",
-    connection: {
-      host: 'localhost',
-      port: '5432',
-      user : 'dev_jn4u_user',
-      password : '32k9f7xYVWQL9yy6tEkRVxU9d6t7JGuF',
-      database : 'dev_jn4u'
-  },
+    connection: DATABASE_URL,
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-  },
-};
+  }
